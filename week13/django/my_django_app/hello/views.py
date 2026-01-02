98e48c92eb6eacf35060a991fa7e42cc34096901
UlwanLuthfi <ulwanxluthfi@gmail.com>
2026-01-02 09:37:54 +0700

    add: week13

---
+++ week13/django/my_django_app/hello/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def salam(request):
+    return HttpResponse("<h1 align='center'>Selamat Belajar Django</h1>")
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def gallery(request):
+    return render(request, "gallery.html")
